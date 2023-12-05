f077512b13f15766637ff406c8ae4c7fbd13c8b4
Leonard392 <leorp392@gmail.com>
2023-12-05 16:33:09 -0500

    last commit in 01 fundamentals Leo

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -20,3 +20,10 @@ javascriptIsFun = "yes";
 const inputYear = "1991";
 console.log(Number(inputYear));
 console.log(inputYear + 18);
+
+// test 4
+
+const bill = 275;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 :
+bill * 0.2;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
